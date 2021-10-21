a9741880bd1e07dbccf63f13b404431bfb5ea4d0
Yinhao Chen <52779413+chenyinhao98@users.noreply.github.com>
2021-10-22 01:56:29 +1100

    Update send_test_email.py

--- facial_recognition/send_test_email.py
+++ facial_recognition/send_test_email.py
@@ -6,10 +6,10 @@ import requests
 def send_simple_message():
     print("I am sending an email.")
     return requests.post(
-        "https://api.mailgun.net/v3/sandboxe1fc3be9bd514e718ee8b9b690652642.mailgun.org/messages",
-        auth=("api", "f5f7cf5393723e903f625de2cbf9743b-443ec20e-d6011f87"),
+        "https://api.mailgun.net/v3/sandbfdgsgdfhdfhsfdasd0652642.mailgun.org/messages",
+        auth=("api", "sfsdf3723esdgsgsg-443ecsdgsdg"),
         data={"from": 'refrigeratorwatcher@example.com',
-              "to":'yche0445@student.monash.edu',
+              "to":'@gmail.com',
             "subject": "Visitor Alert",
             "html": "<html> Your Raspberry Pi recognizes someone. </html>"})
                       
